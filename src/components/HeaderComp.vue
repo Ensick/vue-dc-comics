a300f3a6663d540a7cd2ef75fd4c81c38a116acc
Ensick <fede.censi96@gmail.com>
2022-11-23 14:35:00 +0100

    Componente Header completato

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,94 @@
+
+<template>
+  <header>
+
+    <nav class="header-cont">
+
+      <div>
+        <img src="../assets/img/dc-logo.png" alt="logo">
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <a href="#">CHARACTERS</a>
+          </li>
+          <li>
+            <a href="#">COMICS</a>
+          </li>
+          <li>
+            <a href="#">MOVIES</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">GAMES</a>
+          </li>
+          <li>
+            <a href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a href="#">FANS</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+        </ul>
+      </div>
+
+    </nav>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+}
+</script>
+
+
+<style scoped lang="scss">
+
+  nav{
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+  
+    ul{
+
+      display: flex;
+
+      li{
+
+        list-style-type: none;
+        margin-inline-start: 20px;
+        color: black;
+        padding-bottom: 50px;
+
+        &:hover{
+        
+          color: rgba(12, 124, 236, 1) ;
+          border-bottom: 5px inset rgba(12, 124, 236, 1); 
+        }
+
+        a{
+
+          color: inherit;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+
+
+</style>
